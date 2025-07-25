f4095910c49288294448bd4cff974b246ea5a727
Vishal <kumars701113@gmail.com>
2025-07-25 13:15:48 +0530

    chore: prettier setup

---
+++ prettier.config.js
@@ -0,0 +1,34 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+const config = {
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss", // MUST come last
+  ],
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  endOfLine: "lf",
+  printWidth: 80,
+  semi: true,
+  singleAttributePerLine: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+  //
+  importOrder: [
+    "<BUILTIN_MODULES>",
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "^@jamsr-ui(.*)$",
+    "@/(.*)$",
+    "^[./]",
+    "",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "5.0.0",
+  importOrderCaseSensitive: true,
+};
+
+export default config;
